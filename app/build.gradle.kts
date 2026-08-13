f9c6ccbdc4caeac2ee73cbe5213122143e0abfe9
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-08-13 08:10:43 +0700

    build: bump version to 0.1.3-alpha.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.twinpath.cambg_record"
     minSdk = 24
     targetSdk = 35
-    versionCode = 3
-    versionName = "0.1.2"
+    versionCode = 4
+    versionName = "0.1.3-alpha.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
