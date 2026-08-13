6c0d20b1e1801aa6d09c7b435940df152be3571b
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-08-13 18:15:05 +0700

    config: set base and site for github pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://twinpath.github.io",
+  base: "/cambg/",
   vite: {
     plugins: [tailwindcss()],
   },
