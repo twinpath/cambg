98fc9b46aa7ebe9a382589ddf5481598a9f6fc48
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-08-14 08:32:15 +0700

    feat: add utility to identify system architecture from filenames and client environment

--- docs/src/lib/architecture.ts
+++ docs/src/lib/architecture.ts
@@ -33,7 +33,7 @@ export async function getClientArch(): Promise<string> {
   
   try {
     const canvas = document.createElement("canvas");
-    const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    const gl = (canvas.getContext("webgl") || canvas.getContext("experimental-webgl")) as WebGLRenderingContext | null;
     if (gl) {
       const debugInfo = gl.getExtension("WEBGL_debug_renderer_info");
       if (debugInfo) {
