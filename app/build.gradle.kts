2ef8e310fc1c16554ea1da477d6dea231d3bd552
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-08-14 11:03:15 +0700

    build: bump version to 0.1.7-alpha.2 (code 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.twinpath.cambg"
     minSdk = 24
     targetSdk = 35
-    versionCode = 9
-    versionName = "0.1.7-alpha.1"
+    versionCode = 10
+    versionName = "0.1.7-alpha.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
